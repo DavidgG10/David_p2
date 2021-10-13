407ba099c861c42a8a96063dd9949b5e3b87aec0
DavidG <72959512+DavidgG10@users.noreply.github.com>
2021-10-13 16:31:01 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int num1=0;//se crea una variable 
+int num2=0;// se crea otra varible 
+int result=0;// se crea una variable
+int result_2=0;// se crea una variable
+cout <<"Ingrese un número\n"; 
+cin >>num1;// se le asigna un valor a la variable
+cout << "Ingrese otro múmero\n";
+cin >> num2; // se le asigna un valor a la variable
+result=num1+num2; 
+cout<<"Resultado de la suma\n";
+result=num1+num2;
+cout << result << "\n";
+cout<<"Resultado de la resta\n";
+result=num1-num2;
+cout << result <<"\n";
+
+}
